2dd719ea73a85dd6ccd00f0619e68bd3364abacb
Uyai-Abasi <lorddansco@gmain.com>
2023-08-23 16:17:50 +0100

    first commit

---
+++ alphabets.js
@@ -0,0 +1,12 @@
+var i =65;
+var j =91;
+for(k = i; k < j; k++){
+    var str = String.fromCharCode(k);
+    console.log(str);
+}
+var w =97;
+var x = 123;
+for(y = w; y < x; y++){
+    var str = String.fromCharCode(y);
+    console.log(str);
+}
